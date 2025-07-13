a0bb203acb92d7fc92f78992e365c6dac94ed1d4
Ori <orioh15@gmail.com>
2025-07-13 17:01:22 -0400

    Implemented a pdf parser file to extract text from user uploaded pdf documents

---
+++ server/ml/pdf_parser.py
@@ -0,0 +1,9 @@
+import fitz
+
+# extracts all the text from a pdf file when a user uploads their resume 
+def extract_text_from_pdf(file):
+    doc = fitz.open(stream=file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
