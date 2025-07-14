537fcc9a1ffd077a87c4ea2875ee83ea3f7fa9ff
Ori <orioh15@gmail.com>
2025-07-14 17:42:20 -0400

    implemented lemmatizing to preprocessing file

--- server/ml/preproccesing.py
+++ server/ml/preproccesing.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 def preprocess_text(text):
     text = text.lower()
@@ -10,4 +11,7 @@ def preprocess_text(text):
     
     filtered_list = [word for word in tokens if word.casefold() not in stop_words and word.isalnum()]
 
-    return " ".join(filtered_list)
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_list]
+
+    return " ".join(lemmatized_words)
