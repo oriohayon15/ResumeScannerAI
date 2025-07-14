1e19944e5f3ae9f85e60c4f33dd31317a6725742
Ori <orioh15@gmail.com>
2025-07-14 17:50:09 -0400

    Added preprocessing to backend route

--- server/routes/match.py
+++ server/routes/match.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, request, jsonify
 from ml.pdf_parser import extract_text_from_pdf
+from ml.preprocesing import preprocess_text
 
 match_blueprint = Blueprint('match', __name__)
 
 @match_blueprint.route("/extract", methods=["POST"])
 def extract_text():
     if "resume" not in request.files:
-        return jsonify({"error: No Resume File Uploaded"}), 400
+        return jsonify({"error": "No Resume File Uploaded"}), 400
     
     resume_pdf = request.files["resume"]
-    text = extract_text_from_pdf(resume_pdf)
-    return jsonify({"text": text})
+
+    raw_text = extract_text_from_pdf(resume_pdf)
+
+    clean_text = preprocess_text(raw_text)
+    
+    return jsonify({"text": clean_text})
+
