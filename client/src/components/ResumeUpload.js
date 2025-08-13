120612da1b0d619a8a29c5e70073e96ccad0ae1a
Ori <orioh15@gmail.com>
2025-08-13 17:13:39 -0400

    continued working on resume upload component

--- client/src/components/ResumeUpload.js
+++ client/src/components/ResumeUpload.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
 
+const UPLOAD_URL = "http://localhost:5000/extract";
+
 const ResumeUpload = () => {
 
+    const [file, setFile] = useState()
+    const [error, setError] = useState("")
+    const [uploading, setUploading] = useState(false)
+
+    const handleFileChange = (event) => {
+
+    }
+
+    const handleUploading = () => {
+
+    }
+
     return (
-        <div>Upload</div>
+        <div>Upload your resume (PDF)</div>
     );
 };
 
