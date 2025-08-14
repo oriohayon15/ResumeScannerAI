274fb0710d88ecfc7a0fbeeef4719d38a5a4869c
Ori <orioh15@gmail.com>
2025-08-14 12:02:44 -0400

    implemented the handle upload handler to send the resume file to backend

--- client/src/components/ResumeUpload.js
+++ client/src/components/ResumeUpload.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const UPLOAD_URL = "http://127.0.0.1:5000/extract";
 const MAX_SIZE_MB = 10; 
@@ -31,7 +32,7 @@ const ResumeUpload = () => {
         //size check
         const maxBytes = MAX_SIZE_MB * 1024 * 1024;
         if (chosen.size > maxBytes) {
-            setError(`File to large must be below ${MAX_SIZE_MB} MB.`);
+            setError(`File too large must be below ${MAX_SIZE_MB} MB.`);
             setFile(null)
             return
         }
@@ -42,7 +43,38 @@ const ResumeUpload = () => {
     }
 
     const handleUpload = () => {
+        //Making sure file is submitted
+        if (!file) {
+            setError("Please select a PDF file first.");
+            return
+        }
+        else {
+            setError("");
+            setUploading(true);
+        }
 
+        //Building form data 
+        const form = new FormData();
+        form.append("resume", file);
+        form.append("job", "temp job description") //temp hardcoded 
+
+        //sending the form to the backend
+        axios.post(UPLOAD_URL, form, {
+            onUploadProgress: (progressEvent) => {
+                const percent = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                console.log(`Upload progress: ${percent}%`);
+            }
+        })
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((err) => {
+            setError("Upload failed. Please try again.");
+            console.log(err);
+        })
+        .finally(() => {
+            setUploading(false)
+        });
     }
 
     return (
